9beb99ecd4f7daec81a4ea80810af65e93d7850b
dy6578ekdbs <dy6578ekdbs@gmail.com>
2025-08-14 19:38:35 +0900

    feat: mdx 사용을 위한 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -18,6 +19,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // 이후 필요한 마크다운 플러그인 추가할 수 있다.
+})
+
+export default withMDX(nextConfig);
